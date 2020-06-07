21d38391b962b027d23f6ace54fbd3ee1c31cc15
MateusBMP <butcraft@gmail.com>
2020-06-06 21:25:18 -0300

    Retirando carregamento automatico de pagina

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,5 @@ mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/home.scss', 'public/css')
    .extract(['vue'])
-   // .sourceMaps()
-   .browserSync('127.0.0.1:8000')
+   .sourceMaps()
    .disableNotifications();
