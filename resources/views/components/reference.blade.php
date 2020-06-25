5188774eb502c424368b8cac65ee2f76b2354ffc
MateusBMP <butcraft@gmail.com>
2020-06-25 00:34:25 -0300

    imagens adicionadas

--- resources/views/components/reference.blade.php
+++ resources/views/components/reference.blade.php
@@ -1,3 +1,3 @@
 <div class="reference position-absolute">
-    <span class="mr-1">"Icons made by <a href="https://www.flaticon.com/authors/freepik">Freepik</a> from <a href="http://www.flaticon.com/">www.flaticon.com</a>"</span>
+    <span class="mr-1">"Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="http://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>"</span>
 </div>
